e618b90e8c1a9d350c4b6d68f608c93570735122
Jinpark1049 <92414984+Jinpark1049@users.noreply.github.com>
2025-03-20 15:57:56 +0900

    Update __init__.py

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,3 +1,3 @@
-from .utils import ensure_model, get_ollama_models, get_ngrok_url, check_ollama, get_page, pages_to_text, extract_table_of_content, year_pdf_files
+from .utils import ensure_model, get_ollama_models, get_ngrok_url, check_ollama, get_page, pages_to_text, extract_table_of_content, year_pdf_files, get_ollama_model_names
 from .app_page import load_embeddings, load_parser, main_page, first_page
 from .llm_parser import *  
